ac223f2fbd5f7c3ce9cc9473ea2eeb44f3a167fb
Evgen <evgen.990029@gmail.com>
2020-11-06 20:40:35 +0300

    useEffect, useState, useContext, Loading, Cheked, AddTodo

---
+++ src/Todo/AddTodo.js
@@ -0,0 +1,43 @@
+import React, {useState} from 'react'
+import PropTypes from 'prop-types'
+
+//свой хук
+function useInputValue(defaultValue = ''){
+    const [value, setValue] = useState(defaultValue)
+
+    return{
+        bind: {
+            value,
+            onChange: event => setValue(event.target.value)
+        },
+        clear: () => setValue(''),
+        value: () => value
+    }
+}
+//end
+function AddTodo({onCreate}) {
+    const input = useInputValue('')
+   
+
+    function submitHandler(event) {
+        event.preventDefault()
+
+        if(input.value().trim()){
+            onCreate(input.value())
+            input.clear()
+        }
+    }
+
+    return(
+        <form action="" style={{marginBottom: '1rem'}} onSubmit={submitHandler}>
+            <input type="text" {...input.bind} />
+            <button type="submit">Add</button>
+        </form>
+    )
+}
+
+AddTodo.propTypes = {
+    onCreate: PropTypes.func.isRequired
+}
+
+export default AddTodo
